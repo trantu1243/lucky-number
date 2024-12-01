3f553540f45355c90ccafb7057725c127f38ed52
trantu1243 <trantu1242003@gmail.com>
2024-12-01 16:34:03 +0700

    hosting

--- hosting/index.js
+++ hosting/index.js
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/login', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/sign-up', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'sign-up.html'));
+});
+
 const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
