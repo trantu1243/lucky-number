a4ffc8ba64ebb582ac978ac530c82b109bb6d2d5
trantu1243 <trantu1242003@gmail.com>
2024-12-14 07:51:09 +0700

    webapp

--- bot/routes/paymentSevice.route.js
+++ bot/routes/paymentSevice.route.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const { paymentSeriveController } = require('../controllers');
+const PaymentService = require('../models/paymentService.model');
 
 const router = express.Router();
 
 router.get('/currencies', paymentSeriveController.getAllCurrencies);
 
 router.get('/networks/:currency', paymentSeriveController.getNetworkByCurrency);
+
+router.get('/all', async (req, res) => {
+    const all = await PaymentService.find({});
+    res.send(all);
+});
+
 
 module.exports = router;
