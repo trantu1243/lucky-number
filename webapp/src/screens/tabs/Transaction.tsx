2c5bf2dccb1f5d181d62b21e2d924d6bf9b93742
trantu1243 <trantu1242003@gmail.com>
2025-01-02 17:44:56 +0700

    payout

--- webapp/src/screens/tabs/Transaction.tsx
+++ webapp/src/screens/tabs/Transaction.tsx
@@ -169,7 +169,7 @@ export const Transaction: React.FC = () => {
           title='- Withdraw'
           colorScheme='light'
           containerStyle={{width: '48%'}}
-          onClick={() => navigate('/withdraw')}
+          onClick={() => navigate('/add-payout')}
         />
       </div>
     );
