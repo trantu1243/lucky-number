530f484b127328ca560dbf215f17eaf597c34b4f
trantu1243 <trantu1242003@gmail.com>
2025-01-02 17:49:33 +0700

    payout

--- bot/models/index.js
+++ bot/models/index.js
@@ -2,4 +2,3 @@ module.exports.User = require('./user.model');
 module.exports.History = require('./history.model');
 module.exports.DailyTask = require('./dailyTask.model');
 module.exports.Payment = require('./payment.model');
-module.exports.Rate = require('./rate.model');
