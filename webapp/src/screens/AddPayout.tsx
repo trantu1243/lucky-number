aa172df2e657e8ed8be2a00762b06a94b8950257
Tran Tu <trantu1242003@gmail.com>
2025-01-10 16:39:44 +0700

    transaction

--- webapp/src/screens/AddPayout.tsx
+++ webapp/src/screens/AddPayout.tsx
@@ -114,7 +114,7 @@ export const AddPayout: React.FC = () => {
         }
     }
 
-    function handleDelete(event: React.MouseEvent<HTMLButtonElement, MouseEvent>, addressToDelete: string){
+    function handleDelete(event: React.MouseEvent<SVGSVGElement, MouseEvent>, addressToDelete: string){
         event.preventDefault();
         setLoading(true);
 
@@ -204,6 +204,7 @@ export const AddPayout: React.FC = () => {
                                     fill: 'red', 
                                     marginLeft: 'auto'
                                 }}
+                                onClick={(e) => {handleDelete(e, card.address)}}
                             >
                                 <path d="M 10 2 L 9 3 L 4 3 L 4 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 5 7 L 5 22 L 19 22 L 19 7 L 5 7 z M 8 9 L 10 9 L 10 20 L 8 20 L 8 9 z M 14 9 L 16 9 L 16 20 L 14 20 L 14 9 z" />
                             </svg>
